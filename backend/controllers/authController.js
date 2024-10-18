8577bfb8a32aacbc2942ba27f1f2d4ef96180ac4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-18 17:43:32 +0530

    traveller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,12 +6,12 @@ const register = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const userExists = await User.findOne({ email });
-        if (userExists) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
+        if (userExists) return res.status(400).json({ message: 'User already exists' });
+
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({ name, email, password: hashedPassword });
-        res.status(201).json({ user });
+
+        res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
