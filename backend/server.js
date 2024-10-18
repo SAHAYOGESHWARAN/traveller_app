bb62dc876338eba690bcae52098dec1775fc8f0c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-18 17:21:25 +0530

    traveller

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const postRoutes = require('./routes/postRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/messages', messageRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
